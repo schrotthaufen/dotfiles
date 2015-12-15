13366669e6e6c837611541a26702af4044fed3ce
schrotthaufen <dont@contact.me>
2015-12-15 02:11:13 +0100

    Switched to plain XMonad
    No more plasma-desktop. Just plain ol' XMonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,7 @@
 import XMonad
-import XMonad.Config.Kde
 import qualified XMonad.StackSet as W -- to shift and float windows
 import XMonad.Hooks.ManageHelpers
---import XMonad.Hooks.SetWMName
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.NoBorders
 import XMonad.Layout.IM
 import XMonad.Layout.PerWorkspace
@@ -10,53 +9,68 @@ import XMonad.Layout.Reflect
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.WindowProperties (getProp32s)
 import Data.Ratio ((%))
+import Data.Monoid
+import Control.Monad
 import XMonad.Util.EZConfig -- for additional keybindings
 
-kdeOverride :: Query Bool
-kdeOverride = ask >>= \w -> liftX $ do
-  override <- getAtom "_KDE_NET_WM_WINDOW_TYPE_OVERRIDE"
-  wt <- getProp32s "_NET_WM_WINDOW_TYPE" w
-  return $ maybe False (elem $ fromIntegral override) wt
+main = xmonad $ ewmh defaultConfig 
+	{ modMask = mod4Mask -- use the Windows button as mod
+		, terminal = "/usr/bin/urxvtc"
+		, handleEventHook = fullscreenEventHook <+> removeBordersEventHook
+		, startupHook = myStartupHook
+		, manageHook = myManageHook
+	} `additionalKeys`
+		[ ((mod4Mask .|. shiftMask, xK_l), spawn "/usr/bin/slock")
+		, ((mod4Mask .|. shiftMask, xK_s), spawn "/usr/bin/pwsafe")
+		, ((mod4Mask .|. shiftMask, xK_m), spawn "/usr/bin/claws-mail")
+		, ((mod4Mask, xK_p), spawn "/usr/bin/dmenu_run")
+		]
+	where
+		myManageHook = composeAll . concat $
+			[ [ isFullscreen --> doFullFloat ]
+			, [ className   =? c --> doFloat           | c <- myFloats ]
+			, [ className   =? c --> doF (W.shift "2") | c <- browser ]
+			, [ className   =? c --> doF (W.shift "3") | c <- chat ]
+			, [ className   =? c --> doF (W.shift "4") | c <- mail ]
+			, [ className   =? c --> doF (W.shift "5") | c <- multimedia ]
+			, [ className   =? c --> doF (W.shift "6") | c <- downloads ]
+			, [ className   =? c --> doF (W.shift "7") | c <- games ]
+			, [ className   =? c --> doF (W.shift "8") | c <- rdesktop ]
+			, [ className   =? c --> doF (W.shift "9") | c <- vmcontrol ]
+			]
+		myFloats      = ["Plasma", "Wine", "Super Hexagon", "mpv", "TIS-100"] -- i'm floating
+		browser       = ["chromium", "Firefox", "Tor Browser"] -- open on desktop 2
+		chat          = ["Xchat", "Skype"] -- open on desktop 3
+		mail          = ["Claws-mail"] -- open on desktop 4
+		multimedia    = ["MComix", "mpv", "Clementine", "Sonata", "Spotify"]  -- open on desktop 5
+		downloads     = ["Ktorrent"]
+		games         = ["Steam", "Desura"]
+		rdesktop      = ["rdesktop"] -- open on desktop 8
+		vmcontrol     = ["Virt-manager"]
 
-main = xmonad $ ewmh kde4Config 
- { modMask = mod4Mask -- use the Windows button as mod
- , terminal = "urxvtc"
- , handleEventHook    = fullscreenEventHook
--- , startupHook = setWMName "LG3D" -- fool java
-, layoutHook = myLayoutHook
- , manageHook = ((className =? "krunner") >>= return . not --> manageHook kde4Config)
-                <+> myManageHook
-                <+> (kdeOverride --> doFloat)
- }`additionalKeys`
-   [ ((mod4Mask .|. shiftMask, xK_l), spawn "slock")
-   , ((mod4Mask .|. shiftMask, xK_s), spawn "pwsafe")
-   ]
- where
-   myManageHook = composeAll . concat $
-     [ [ isFullscreen --> doFullFloat ]--(doF W.focusDown <+> doFullFloat) ]
-     , [ className   =? c --> doFloat           | c <- myFloats ]
-     , [ className   =? c --> doF (W.shift "2") | c <- browser ]
-     , [ className   =? c --> doF (W.shift "3") | c <- chat ]
-     , [ className   =? c --> doF (W.shift "4") | c <- mail ]
-     , [ className   =? c --> doF (W.shift "5") | c <- multimedia ]
-     , [ className   =? c --> doF (W.shift "6") | c <- downloads ]
-     , [ className   =? c --> doF (W.shift "7") | c <- games ]
-     , [ className   =? c --> doF (W.shift "8") | c <- rdesktop ]
-     , [ className   =? c --> doF (W.shift "9") | c <- vmcontrol ]
-     , [ className   =? c --> doIgnore          | c <- myIgnores ]
-     ]
-   myIgnores     = ["desktop_window", "kdesktop"] -- just don't care about those ;)
-   myFloats      = ["Plasma", "Wine", "Super Hexagon", "mpv", "TIS-100"] -- i'm floating
-   browser       = ["Chromium", "Firefox", "Tor Browser"] -- open on desktop 2
-   chat          = ["Xchat", "Skype"] -- open on desktop 3
-   mail          = ["Claws-mail"] -- open on desktop 4
-   multimedia    = ["MComix", "mpv", "Clementine", "Sonata", "Last.fm", "Spotify"]  -- open on desktop 5
-   downloads     = ["Ktorrent"]
-   games         = ["Steam", "Desura"]
-   rdesktop      = ["rdesktop"] -- open on desktop 8
-   vmcontrol     = ["Virt-manager"]
+		myStartupHook = do
+			setWMName "LG3D" -- fool java
+			spawn "/home/schrotthaufen/.xmonad/startup.sh"
 
-   myLayoutHook = smartBorders . onWorkspace "Gimp" gimp $ layoutHook kde4Config
-   gimp = withIM (0.11) (Role "gimp-toolbox") $
-          reflectHoriz $
-          withIM (0.15) (Role "gimp-dock") Full
+		-- from http://funktionaali.com/posts/2014-07-01-How%20to%20get%20XMonad%20play%20well%20with%20fullscreen%20mpv.html
+		-- | Remove borders from every mpv window as soon as possible in an event
+		-- hook, because otherwise dimensions are messed and the fullscreen mpv is
+		-- stretched by a couple pixels.
+		--
+		-- Basically the effect is the same as with
+		-- "XMonad.Layout.NoBorders.lessBorders OnlyFloat", except that OnlyFloat
+		-- messes up the dimensions when used together with fullscreenEventHook
+		-- (e.g. NET_WM_STATE). Well at least in mplayer/mpv.
+		--
+		-- I have no idea how often/where the border is re-applied, but resetting
+		-- it to 0 whenever possible just works :)
+		removeBordersEventHook :: Event -> X All
+		removeBordersEventHook ev = do
+			whenX (className =? "mpv" `runQuery` w) $ withDisplay $ \d -> do
+				cw <- io $ wa_border_width <$> getWindowAttributes d w
+				unless (cw == 0) $ do
+					io $ setWindowBorderWidth d w 0
+					refresh
+			return (All True)
+			where
+				w = ev_window ev
